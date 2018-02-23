a489d792e12bf3ac274f2e484d4d172da5c4dfcc
RohitG28 <rohit.ghivdonde@gmail.com>
2018-02-23 10:15:41 +0530

    Changed atoi to atol

--- src/finalSieve.c
+++ src/finalSieve.c
@@ -1,7 +1,11 @@
 #include "mpi.h"
 #include <iostream>
+
+//For using ceil function
 #include <math.h>
 #include <stdio.h>
+
+//For File I/O
 #include <fstream>
 #include <stdlib.h>
 #include <bits/stdc++.h>
@@ -17,6 +21,7 @@ int main(int argc, char** argv)
 	//Parallelization starts
 	err = MPI_Init(&argc, &argv);
 
+	//Time calculation starts
 	elapsedTime = -MPI_Wtime();
 	
 	//Get Rank and Total no of processes
@@ -30,42 +35,49 @@ int main(int argc, char** argv)
 	char* marked2;
 
 	//variable N
-	long int n = atoi(argv[1]);
+	long int n = atol(argv[1]);
 
+	//Vector stores primes computed by the current process
 	vector<long int> primes;
 	
+	//Square root of N
 	long int sqrtN = ceil((double)sqrt(n));
 
+	//If square root of N is even, make it odd by subtracting 1
 	if(sqrtN%2 == 0)
 	{
 		sqrtN = sqrtN - 1;
 	}
 
+	//Number of elements to be assigned to each process
 	long int blockSize;
+
+	//Lowest, highest element assigned to a process and the remaining elements after division
 	long int low,high,remainder;
 
+	//Allocate required space
 	marked1 = (char*)malloc((((sqrtN-3)/2)+1)*sizeof(char));
 
-	//Size of range given to each process
 	blockSize = (n-(sqrtN))/(noOfProcesses);
 	low = sqrtN + processId*blockSize + 1;
 	high = low + blockSize - 1;
 	remainder = (n-(sqrtN))%(noOfProcesses);
 
+	//Put the remaining elements in the last process's block
 	if(processId == noOfProcesses-1)
 	{
 		high += remainder;
 	}
 
-	//if first number assigned to a process is even then move to next odd number
+	//If first number assigned to a process is even then move to next odd number
 	if(low%2 == 0)
 		low++;
 
-	//if last number assigned to a process is odd then move one number back
+	//If last number assigned to a process is odd then move one number back
 	if(high%2 == 0)
 		high--;
 
-	//divide the marked2 array by 2 so as to remove all even numbers
+	//Divide the marked2 array by 2 so as to remove all even numbers
 	long int highIndex = (high-low)/2;
 	long int sieveStart;
 	long int j = 0; 
@@ -74,30 +86,26 @@ int main(int argc, char** argv)
 
 	int rootProcess = 0;
 
-	//initialize all numbers to be prime
+	//Initialize all numbers as prime
 	memset(marked1, '0', (((sqrtN-3)/2)+1));
 	memset(marked2, '0', (((high-low)/2)+1));
 
+	//Files to store prime numbers
 	ofstream primesFile, statFile;
 	char processIdStr[3];
 	sprintf(processIdStr,"%d",processId);
 	
-	//let all processes reach this point 
+	//Let all processes reach this point 
 	MPI_Barrier(MPI_COMM_WORLD);
 	long int primeNo;
 
-	//mark all even numbers except 2 in marked1 as non prime 
-	// for(long int k=4;k<=sqrtN;k+=2)
-	// {
-	// 	marked1[k] = '1';
-	// }
-
 	if(processId == rootProcess)
 	{
+		//First push 2 into the primes array of root process
 		primes.push_back(2);
 	}
 
-	//for each unmarked number in marked1 sieve marked1 as well as marked2 arrays thus marking non primes
+	//For each unmarked number in marked1, sieve marked1 as well as marked2 arrays thus marking non primes
 	for(long int i=0; i<=(sqrtN-3)/2; i++)
 	{
 		if(marked1[i] == '0')
@@ -114,17 +122,17 @@ int main(int argc, char** argv)
 				marked1[k] = '1';
 			}
 			
-			//find the first number(obviously odd) divisible by the current prime in the process range
+			//Find the first number(obviously odd) divisible by the current prime in the process range
 			sieveStart = (low/primeNo)*primeNo;
 			
 			if(sieveStart<low)
 				sieveStart = sieveStart+primeNo;
 
-			//if the number is even, again add the prime number to get its first odd multiple in the range
+			//If the number is even, again add the prime number to get its first odd multiple in the range
 			if(sieveStart%2 == 0)
 				sieveStart += primeNo;
 
-			//the number from which sieve starts must be less than the last number in the range
+			//The number from which sieve starts must be less than the last number in the range
 			if(sieveStart <= high)
 			{
 				j = (sieveStart - low)/2;
@@ -137,6 +145,7 @@ int main(int argc, char** argv)
 		}
 	}
 
+	//Now push the prime numbers into primes array of each process
 	for(long int i=0; i<=highIndex; i++)
 	{
 		if(marked2[i] == '0')
@@ -144,13 +153,17 @@ int main(int argc, char** argv)
 			primes.push_back(low + i*2);
 		}
 	}
+
+	//Time calculation done
 	elapsedTime += MPI_Wtime();
 
+	//Free the arrays
 	free(marked1);
 	free(marked2);
 
 	MPI_Barrier(MPI_COMM_WORLD);
 
+	//Write the primes into files
 	primesFile.open (processIdStr, std::ofstream::out | std::ofstream::trunc);
 
 	for(long int i=0; i<primes.size(); i++)
@@ -159,15 +172,6 @@ int main(int argc, char** argv)
 	}
 	
 	primesFile.close();
-
-	if(processId == rootProcess)
-	{
-		statFile.open ("statfile", std::ofstream::out | std::ofstream::trunc);
-		statFile << n << "\n";
-		statFile << noOfProcesses << "\n";
-		statFile << elapsedTime << "\n";
-		statFile.close();
-	}
 
 	printf("total time take by %d : %lf\n",processId,elapsedTime); 
 
